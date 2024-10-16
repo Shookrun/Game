979bb58df9c67bb76e5cb2f82ba8c70e3fa8c200
Shookrun <sukranmamedov999@gmail.com>
2024-10-16 19:44:52 +0400

    PinPong

---
+++ app.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const main = document.getElementById("main");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+
+  
+    if (menu.classList.contains("hidden")) {
+        setTimeout(() => {
+            main.style.display = "flex";
+        }, 2000);
+    } else {
+    
+        main.style.display = "none";
+    }
+});
